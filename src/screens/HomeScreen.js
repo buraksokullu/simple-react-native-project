e102d7cde8d01c4e86db6ce847832f130c31e777
Burak Sokullu <burak.sokullu@hepsiburada.com>
2021-01-02 21:06:10 +0300

    image screen and counter

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,43 @@
 import React from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hi World!</Text>
-      <Button 
-      onPress={() => console.log('tıklandı!!')}
-      title="Go to components Demo!" />
+      <Button
+        onPress={() => navigation.navigate("Components")}
+        title="Go to components Demo!"
+      />
+
+      <Button
+        onPress={() => navigation.navigate("List")}
+        title="Go to components List!"
+      />
+
+      <Button
+        onPress={() => navigation.navigate("Image")}
+        title="Go to components Image!"
+      />
+
+      <Button
+        onPress={() => navigation.navigate("Counter")}
+        title="Go to components Counter!"
+      />
+
+      <Button
+        onPress={() => navigation.navigate("Color")}
+        title="Go to components Color!"
+      />
+
+      <Button
+        onPress={() => navigation.navigate("Square")}
+        title="Go to components Square!"
+      />
+
+      <TouchableOpacity onPress={() => navigation.navigate("List")}>
+        <Text> Go to List Demo</Text>
+      </TouchableOpacity>
     </View>
   );
 };
